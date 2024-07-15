d50ab5f4842e2b5905f32936391911bad3aa02a0
Red Oral <redmunozoral@gmail.com>
2024-07-14 22:43:23 -0700

    Updated lily58 keymap, updated sofle oled and fixed rgb

--- keyboards/lily58/keymaps/redoral/keymap.c
+++ keyboards/lily58/keymaps/redoral/keymap.c
@@ -41,28 +41,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |  +=  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            | LGui | LAlt |MO(1) | /Space  /       \Enter \  |MO(1) |  ~`  |      |
+ *            | LGui | LAlt |MO(1) | /Space  /       \Enter \  |MO(1) |  ~`  |   \  |
  *            |      |      |      |/       /         \      \ |      |      |      |
  *            ----------------------------'           '------''----------------------
  */
 [0] = LAYOUT(
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                    KC_6,    KC_7,    KC_8,     KC_9,   KC_0,     KC_MINS,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                    KC_Y,    KC_U,    KC_I,     KC_O,   KC_P,     KC_BSPC,
-  KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                    KC_H,    KC_J,    KC_K,     KC_L,   KC_SCLN,   KC_QUOT,
+  KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                    KC_H,    KC_J,    KC_K,     KC_L,   KC_SCLN,  KC_QUOT,
   KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,   KC_RBRC,KC_N,    KC_M,    KC_COMM,  KC_DOT, KC_SLSH,  KC_EQL,
-                              KC_LGUI, KC_LALT, MO(1), KC_SPC,      KC_ENT,  MO(1), KC_GRV, XXXXXXX
+                              KC_LGUI, KC_LALT, MO(1), KC_SPC,      KC_ENT,  MO(1), KC_GRV, KC_BSLS
 ),
 
 /*
  * MO(1)
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |  `   |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  |  F11 |
+ * |  ~`  |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  |  F11 |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      | Home |  Up  | End  |      |      |                    |  Ins |  Del |  MsU |   [  |   ]  |  F12 |
+ * |      | Home |  Up  | End  | PgUp | Vol+ |                    |  Ins |  Del |  MsU |   [  |   ]  |  F12 |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |  Lf  |  Dn  |  Rt  |      |      |-------.    ,-------|  Ms1 |  MsL |  MsD |  MsR |  Ms2 |      |
+ * |      |  Lf  |  Dn  |  Rt  | PgDwn| Vol- |-------.    ,-------|  Ms1 |  MsL |  MsD |  MsR |  Ms2 |      |
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------|    |-------| SclU | SclD |      |      |      |      |
+ * |      |      |      | Prev | Next | Play |-------|    |-------| SclU | SclD |      |      |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *            |      |      |      | /       /       \      \  |      |      |      |
  *            |      |      |      |/       /         \      \ |      |      |      |
@@ -70,10 +70,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [1] = LAYOUT(
-  KC_TRNS, KC_F1,   KC_F2,    KC_F3,    KC_F4,   KC_F5,                       KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,
-  KC_TRNS, KC_HOME, KC_UP,    KC_END,   KC_TRNS, KC_TRNS,                     KC_INS,   KC_DEL,   KC_MS_U,  KC_LBRC,  KC_RBRC,  KC_F12,
-  KC_TRNS, KC_LEFT, KC_DOWN,  KC_RGHT,  KC_TRNS, KC_TRNS,                     KC_BTN1,  KC_MS_L,  KC_MS_D,  KC_MS_R,  KC_BTN2,  KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_WH_U,  KC_WH_D,  KC_BTN3,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+  KC_GRV,  KC_F1,   KC_F2,    KC_F3,    KC_F4,   KC_F5,                        KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,    KC_F11,
+  KC_TRNS, KC_HOME, KC_UP,    KC_END,   KC_PGUP, KC_VOLU,                     KC_INS,   KC_DEL,   KC_MS_U,  KC_LBRC,  KC_RBRC,  KC_F12,
+  KC_TRNS, KC_LEFT, KC_DOWN,  KC_RGHT,  KC_PGDN, KC_VOLD,                     KC_BTN1,  KC_MS_L,  KC_MS_D,  KC_MS_R,  KC_BTN2,  KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS,  KC_MPRV,  KC_MNXT, KC_MPLY, KC_TRNS,   KC_TRNS, KC_WH_U,  KC_WH_D,  KC_BTN3,  KC_TRNS,  KC_TRNS,  KC_TRNS,
                          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 ),
 
@@ -95,7 +95,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 ),
 
@@ -117,7 +117,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 )
 };
