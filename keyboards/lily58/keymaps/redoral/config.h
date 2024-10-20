f27acd18ce6f98f86f935a80ffd3ee04276a003b
Red Oral <redmunozoral@gmail.com>
2024-10-19 22:57:03 -0700

    updated oled, cleaned up config for lily58

--- keyboards/lily58/keymaps/redoral/config.h
+++ keyboards/lily58/keymaps/redoral/config.h
@@ -20,20 +20,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-//#define USE_MATRIX_I2C
-
-/* Select hand configuration */
-
-#define MASTER_LEFT
-// #define MASTER_RIGHT
-// #define EE_HANDS
-
+#define SPLIT_USB_DETECT
 #define QUICK_TAP_TERM 0
 #define TAPPING_TERM 100
-
-// Underglow
-/*
-#undef RGBLIGHT_LED_COUNT
-#define RGBLIGHT_LED_COUNT 14    // Number of LEDs
-#define RGBLIGHT_SLEEP
-*/
